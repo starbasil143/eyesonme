844fc1bfbd0e8a096fdb30b41998224fd69fb4ba
starbasil <167671623+starbasil143@users.noreply.github.com>
2025-02-25 02:20:01 -0600

    SHIFT aiming, ready light beam

--- Assets/Input/InputManager.cs
+++ Assets/Input/InputManager.cs
@@ -3,21 +3,40 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public static Vector2 Movement;
-
     private PlayerInput _playerInput;
+
+    
+    public static Vector2 Movement;
     private InputAction _moveAction;
+
+    public static bool Ready;
+    private InputAction _readyAction;
+
+    public static Vector2 Aim;
+    private InputAction _aimAction;
+
+    public static bool Fire;
+    private InputAction _fireAction;
+
 
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
+
         _moveAction = _playerInput.actions["Move"];
+        _aimAction = _playerInput.actions["Aim"];
+        _readyAction = _playerInput.actions["Ready"];
+        _fireAction = _playerInput.actions["Fire"];
     }
 
     private void Update()
     {
         Movement = _moveAction.ReadValue<Vector2>();
+        Aim = _aimAction.ReadValue<Vector2>();
+        Ready = _readyAction.IsPressed();
+        Fire = _fireAction.WasPressedThisFrame();
+
     }
 
 }
--- Assets/_Scripts/PlayerAim.cs
+++ Assets/_Scripts/PlayerAim.cs
@@ -5,22 +5,63 @@ public class PlayerAim : MonoBehaviour
 {
     public GameObject PlayerParent;
     public Camera MainCamera;
+    public Transform _transform;
+    public bool isReady;
+    [SerializeField] private LineRenderer _lineRenderer;
 
 
     void Awake()
     {
         PlayerParent = transform.parent.gameObject;
+        _transform = PlayerParent.transform;
+        _lineRenderer = PlayerParent.GetComponentInChildren<LineRenderer>();
+        _lineRenderer.positionCount = 2;
         MainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
 
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 playerPos = PlayerParent.transform.position;
-        
-        Vector2 differenceVector = mousePos - playerPos;
+        if (InputManager.Ready)
+        {
+            ReadyUpdate();
 
-        Debug.Log(differenceVector);   
+            if (!isReady)
+            {
+                isReady = true;
+                _lineRenderer.enabled = true;
+            }
+        }
+        else
+        {
+            if (isReady)
+            {
+                isReady = false;
+                _lineRenderer.enabled = false;
+            }
+        }
+
+        if (InputManager.Fire && isReady)
+        {
+            Fire();
+        }
+    }
+
+
+    private void ReadyUpdate()
+    { 
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(InputManager.Aim);
+        Vector2 playerPos = _transform.position;
+        Vector2 differenceVector = mousePos - playerPos;
+        Vector2 directionVector = differenceVector.normalized;
+
+        _lineRenderer.SetPosition(0, playerPos);
+        _lineRenderer.SetPosition(1, playerPos + directionVector*20);
+
+    }
+
+    private void Fire()
+    {
+        Debug.Log("bang!!!!!");
     }
 }
